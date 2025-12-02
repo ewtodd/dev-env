d8b36dde17ba6c05e629017e1e38388d85eb8652
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-12-02 14:50:26 -0500

    update to account for breaking changes to root paths in 25.11

--- dev-shell/flake.nix
+++ dev-shell/flake.nix
@@ -12,19 +12,23 @@
 
       in {
         devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ pkgconfig ];
           buildInputs = with pkgs; [
             root
-            (python3.withPackages (python-pkgs:
-              with python-pkgs; [
-                matplotlib
-                numpy
-                pandas
-                tables
-                mplhep
-                uproot
-                h5py
-              ]))
+            gnumake
+            (python3.withPackages
+              (python-pkgs: with python-pkgs; [ matplotlib numpy pandas ]))
           ];
+          shellHook = ''
+            STDLIB_PATH="${pkgs.stdenv.cc.cc}/include/c++/${pkgs.stdenv.cc.cc.version}"
+            STDLIB_MACHINE_PATH="$STDLIB_PATH/x86_64-unknown-linux-gnu"
+            export CPLUS_INCLUDE_PATH="$STDLIB_PATH:$STDLIB_MACHINE_PATH:$PWD/include:$(root-config --incdir):$CPLUS_INCLUDE_PATH"
+            export ROOT_INCLUDE_PATH="$PWD/include:$(root-config --incdir)"
+            export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
+
+            echo "C++ stdlib: $STDLIB_PATH"
+            echo "LSP should now work correctly"
+          '';
         };
       });
 }
