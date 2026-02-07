4397ad3e9e6edf6160527577f6c99b4c2e279726
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-02-07 08:24:03 -0500

    update to support zsh

--- dev-shell/flake.nix
+++ dev-shell/flake.nix
@@ -1,33 +1,53 @@
 {
   description = "Nix Development Shell Flake Template";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ pkg-config ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gnumake
+            clang-tools
+          ];
           buildInputs = with pkgs; [
             root
+            zsh
             gnumake
-            (python3.withPackages
-              (python-pkgs: with python-pkgs; [ matplotlib numpy pandas ]))
+            (python3.withPackages (
+              python-pkgs: with python-pkgs; [
+                matplotlib
+                numpy
+                pandas
+              ]
+            ))
           ];
           shellHook = ''
+            export SHELL="${pkgs.zsh}/bin/zsh"
+            echo "ROOT version: $(root-config --version)"
             STDLIB_PATH="${pkgs.stdenv.cc.cc}/include/c++/${pkgs.stdenv.cc.cc.version}"
             STDLIB_MACHINE_PATH="$STDLIB_PATH/x86_64-unknown-linux-gnu"
-            export CPLUS_INCLUDE_PATH="$STDLIB_PATH:$STDLIB_MACHINE_PATH:$PWD/include:$(root-config --incdir):$CPLUS_INCLUDE_PATH"
-            export ROOT_INCLUDE_PATH="$PWD/include:$(root-config --incdir)"
-            export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
-
-            echo "C++ stdlib: $STDLIB_PATH"
+            ROOT_INC="$(root-config --incdir)"
+            # Local first, then remote, then others
+            export CPLUS_INCLUDE_PATH="$PWD/include:$STDLIB_PATH:$STDLIB_MACHINE_PATH:$ROOT_INC''${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}"
+            export ROOT_INCLUDE_PATH="$PWD/include''${ROOT_INCLUDE_PATH:+:$ROOT_INCLUDE_PATH}"
+            # Local lib first means linker will use it preferentially
+            export LD_LIBRARY_PATH="$PWD/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+            exec ${pkgs.zsh}/bin/zsh
           '';
         };
-      });
+      }
+    );
 }
